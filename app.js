ff9d809ab14ef13458d0b3a8f68c2ae7013765e3
asif2212 <asifmd2212@gamil.com>
2023-08-24 16:29:53 +0600

    vue3 js first show

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const app = Vue.createApp({
     data(){
         return {
-            msg: 'Hello, welcome to vue3 js'
+            msg: 'Hello, welcome to vue3 js',
+            githubInfo: {
+                name: "Moshiur Rahman",
+                githubLink: "https://github.com/moshiurasif"
+            },
+            htmlCode: '<a href="githubInfo.githubLink">Github</a>'
         }
     }
 })
