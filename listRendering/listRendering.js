2ef16aae5e31608d9c79f9ffd3ee66af15f7c5aa
Moshiur Asifm <asifmd2212@gmail.com>
2023-08-26 17:14:53 +0600

    list rendering

---
+++ listRendering/listRendering.js
@@ -0,0 +1,32 @@
+const app = Vue.createApp({
+    data(){
+        return {
+          skills:["html", "css", "js"],
+          newSkill:"",
+          products:[
+            {name:'Shirt', price: 30},
+            {name:'Pants', price: 40},
+            {name:'Cap', price: 200},
+        ],
+        newProduct: {}
+        }
+    },
+    methods: {
+      addSkill(){
+        this.skills.push(this.newSkill);
+        this.newSkill = ""
+      },
+      removeSkill(i){
+        this.skills.splice(i, 1)
+      },
+      addProduct(){
+        this.produts.push({name: this.newProduct, price: 0});
+        this.newProduct = {}
+      },
+      removeProduct(i){
+        this.products.splice(i, 1);
+      }
+    }
+})
+
+app.mount('#app')
