75f28827ef918b634cf27de2a6b424388bf8d700
Moshiur Asifm <asifmd2212@gmail.com>
2023-08-26 16:39:07 +0600

    style binding

---
+++ styleBinding/styleBinding.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+    data(){
+        return {
+           textColor: 'white',
+           bgColor: 'green'
+        }
+    },
+    methods: {
+      
+    }
+})
+
+app.mount('#app')
