7830970ec54a3c92e49144a0ee3610a9722412bd
asif2212 <asifmd2212@gamil.com>
2023-08-26 23:06:32 +0600

    list rendering of object and numbers

---
+++ listRenderingObjectAndNumber/app.js
@@ -0,0 +1,17 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            aboutMe:{
+                name: "Moshiur Rahman",
+                nickName: "Asif",
+                website: "http://www.moshiur.com"
+            }
+        }
+    },
+    methods: {
+
+    }
+})
+
+app.mount("#app")
+
