65319ef20f188106c2163ff162001481aa92bdec
asif2212 <asifmd2212@gamil.com>
2023-09-04 12:15:37 +0600

    api fetching by axios

---
+++ apiFetching/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1>Posts</h1>
+        <div v-for="post in posts" :key="post.id">
+            <h2>{{ post.title}}</h2>
+            <p>{{post.body}}</p>
+            <hr>
+        </div>
+        
+    </div>
+</template>
+<script>
+import axios from 'axios';
+export default {
+    
+    data(){
+  return{
+    posts: []
+  }
+},
+
+created(){
+  axios.get("https://jsonplaceholder.typicode.com/posts")
+.then(res => {
+    this.posts = res.data;
+})
+.catch(err => console.log(err))
+}
+}
+</script>
+<style scoped>
+    
+</style>
