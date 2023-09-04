c2de4fd0f4b5bccf56ad57337a971d8dcf34518e
asif2212 <asifmd2212@gamil.com>
2023-09-04 13:36:27 +0600

    filter added to post

--- apiFetching/src/App.vue
+++ apiFetching/src/App.vue
@@ -1,33 +1,30 @@
 <template>
-    <div>
-        <h1>Posts</h1>
-        <div v-for="post in posts" :key="post.id">
-            <h2>{{ post.title}}</h2>
-            <p>{{post.body}}</p>
-            <hr>
-        </div>
-        
+  <div>
+    <h1>Posts</h1>
+    <div v-for="post in posts" :key="post.id">
+      <h2>{{ post.title }}</h2>
+      <p>{{ $filters.snippet(post.body) }}</p>
+      <hr />
     </div>
+  </div>
 </template>
 <script>
-import axios from 'axios';
+import axios from "axios";
 export default {
-    
-    data(){
-  return{
-    posts: []
-  }
-},
+  data() {
+    return {
+      posts: [],
+    };
+  },
 
-created(){
-  axios.get("https://jsonplaceholder.typicode.com/posts")
-.then(res => {
-    this.posts = res.data;
-})
-.catch(err => console.log(err))
-}
-}
+  created() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => {
+        this.posts = res.data;
+      })
+      .catch((err) => console.log(err));
+  },
+};
 </script>
-<style scoped>
-    
-</style>
+<style scoped></style>
--- apiFetching/src/main.js
+++ apiFetching/src/main.js
@@ -1,5 +1,15 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.config.globalProperties.$filters = {
+  snippet(val) {
+    if (!val || typeof val !== 'string') return '';
+    val = val.slice(0, 50);
+    return val;
+  },
+};
+
+app.mount('#app');
